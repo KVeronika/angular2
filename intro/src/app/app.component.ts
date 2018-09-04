458ec70b83b60b818fd16491ad0632ba2dad33fe
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-04 11:44:00 +0400

    directives lesson

--- intro/src/app/app.component.ts
+++ intro/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  'Learn JavaScript',
+  'Learn Angular 2',
+  'Write an application'
+];
+
 @Component({
   selector: 'app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = "Angular 2Do"
+  title = 'Angular 2Do';
+  todos = todos;
 }
