95484f447f4a74c7ad8b0446948386ce92e0ba5f
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-04 18:25:55 +0400

    decomposition lesson

--- intro/src/app/app.component.ts
+++ intro/src/app/app.component.ts
@@ -1,25 +1,5 @@
 import { Component } from '@angular/core';
 
-class Todo {
-  constructor(public title: string,
-              public completed: boolean = false) {}
-}
-
-const todos: Todo[] = [
-  {
-    title: 'Learn JavaScript',
-    completed: true
-  },
-  {
-    title: 'Learn Angular 2',
-    completed: false
-  },
-  {
-    title: 'Write an application',
-    completed: false
-  }
-];
-
 @Component({
   selector: 'app',
   templateUrl: 'app.component.html',
@@ -27,25 +7,4 @@ const todos: Todo[] = [
 })
 export class AppComponent {
   title: string = 'Angular 2Do';
-  todos: Todo[] = todos;
-  newTodoTitle: string = '';
-
-  toggle(todo: Todo) {
-    todo.completed = !todo.completed;
-  }
-
-  delete(todo: Todo) {
-    const index = this.todos.indexOf(todo);
-
-    if (index > -1) {
-      this.todos.splice(index, 1);
-    }
-  }
-
-  create() {
-    let todo: Todo = new Todo(this.newTodoTitle);
-
-    this.todos.push(todo);
-    this.newTodoTitle = '';
-  }
 }
