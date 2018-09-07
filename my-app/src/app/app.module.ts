9225807d72493cebbcc5c5aa9584d786fec53c90
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-07 12:47:38 +0400

    data transfer to the component lesson

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { UserCardComponent } from './header/user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule
