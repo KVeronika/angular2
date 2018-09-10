226bd9a10d01e004da8eb104a49993b364ca61e5
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-10 22:49:32 +0400

    working with parameters

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 
 const routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
+  { path: 'users',
+    data: {
+      title: 'Users',
+      anotherParam: 'Test'
+    },
+    component: UsersComponent },
+  { path: 'users/:userId', component: UserComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
--- my-app/src/app/users/users.component.ts
+++ my-app/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => console.log(params));
+    this.route.data.subscribe(data => console.log(data));
+  }
 
   ngOnInit() {
   }
