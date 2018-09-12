451b8395012d8c605fb5b0afbe31cef06278540c
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-12 14:26:36 +0400

    reactive forms - formControl

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
-  userId = 15;
+  nameControl: FormControl;
+
+  ngOnInit() {
+    this.nameControl = new FormControl('John', [myValidator(5)], [myAsyncValidator]);
+
+    this.nameControl.valueChanges.subscribe(value => console.log(value));
+    this.nameControl.statusChanges.subscribe(status => {
+      console.log(this.nameControl.errors);
+      console.log(status);
+    });
+  }
+}
+
+function myValidator(number) {
+  return function (formControl: FormControl) {
+    if (formControl.value.length < number) {
+      return { myValidator: { message: 'Something went wrong' } };
+    }
+    return null;
+  }
+}
+
+function myAsyncValidator(formControl: FormControl): Observable<null | any> {
+  if (formControl.value.length < 3) {
+    return of({ myAsyncValidator: { message: 'Something went wrong' } });
+  }
+  return of(null);
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,49 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
-import { AuthGuard } from './auth.guard';
-import { UserResolveService } from './user-resolve.service';
-import { AuthService } from './auth.service';
-import { CustomPreloadingStrategy } from './custom-preloading-strategy';
-
-const routes = [
-  { path: '', component: HomeComponent },
-  { path: 'admin',
-/*     data: {
-      nopreload: false
-    }, */
-    loadChildren: './admin/admin.module#AdminModule'},
-  { path: 'users',
-    canActivate: [ AuthGuard ],
-    resolve: {
-      user: UserResolveService
-    },
-    data: {
-      title: 'Some title',
-      anotherParam: 'Another'
-    },
-    component: UsersComponent
-  },
-  { path: 'users/:userId', component: UserComponent }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, { preloadingStrategy: CustomPreloadingStrategy })
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [ AuthGuard, AuthService, UserResolveService, CustomPreloadingStrategy ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
