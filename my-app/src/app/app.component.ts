fc967f1f3abf8ce08124cfddd689c10e28ef46a4
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-12 17:55:45 +0400

    reactive forms - custom element

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
-import { of, Observable } from 'rxjs';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,65 +7,12 @@ import { of, Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'my-app';
-  nameControl: FormControl;
-  fullNameControl: FormGroup;
-  userListControl: FormGroup;
-
-  constructor(private _fromBuilder: FormBuilder) {}
+  title = 'app';
+  trafficControl: FormControl;
 
   ngOnInit() {
-    this.nameControl = new FormControl('John', [myValidator(5)], [myAsyncValidator]);
+    this.trafficControl = new FormControl();
 
-    this.nameControl.valueChanges.subscribe(value => console.log(value));
-    this.nameControl.statusChanges.subscribe(status => {
-      console.log(this.nameControl.errors);
-      console.log(status);
-    });
-
-    this.fullNameControl = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl()
-    });
-
-    this.fullNameControl.valueChanges.subscribe(value => console.log(value));
-
-    // this.userListControl = new FormGroup({
-    //   users: new FormArray([
-    //     new FormControl('Alice'),
-    //     new FormControl('Bob'),
-    //     new FormControl('John')
-    //   ])
-    // });
-
-    this.userListControl = this._fromBuilder.group({
-      users: this._fromBuilder.array([['Alice'], ['Bob'], ['John']])
-    });
-
-    this.userListControl.valueChanges.subscribe(value => console.log(value));
+    this.trafficControl.valueChanges.subscribe((value) => console.log(value));
   }
-
-  removeUserControl(index: number) {
-    (this.userListControl.controls['users'] as FormArray).removeAt(index);
-  }
-
-  addUserControl() {
-    (this.userListControl.controls['users'] as FormArray).push(new FormControl(''));
-  }
-}
-
-function myValidator(number) {
-  return function (formControl: FormControl) {
-    if (formControl.value.length < number) {
-      return { myValidator: { message: 'Something went wrong' } };
-    }
-    return null;
-  }
-}
-
-function myAsyncValidator(formControl: FormControl): Observable<null | any> {
-  if (formControl.value.length < 3) {
-    return of({ myAsyncValidator: { message: 'Something went wrong' } });
-  }
-  return of(null);
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TrafficComponent } from './traffic/traffic.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrafficComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
