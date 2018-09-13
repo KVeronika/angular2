e6d3e79b2d2bb662289b7b2b1184ec14a9943f3a
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-13 13:50:26 +0400

    testing of components

--- testing/src/app/app.module.ts
+++ testing/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReversePipe } from './reverse.pipe';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReversePipe
+    ReversePipe,
+    UserCardComponent
   ],
   imports: [
     BrowserModule
