b07caedb4dccd4048cd0a667ebdc728f818c35f4
Veronika Kuzmina <veronika.cuzmina2011@yandex.ru>
2018-09-25 14:48:26 +0400

    highlighting of nav items added

--- ng6/src/app/sidebar/sidebar.component.ts
+++ ng6/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public currentUrl: string;
 
-  ngOnInit() {
+  constructor(private _router: Router) {
+    _router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
   }
+
+  ngOnInit() {}
 
 }
